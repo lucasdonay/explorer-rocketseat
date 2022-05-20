c1a054757aea53e94a3257047ee17f77f01cb2e8
Lucas Donay <lucas.donay@gmail.com>
2022-05-20 18:43:58 -0300

    refactoring - step 1

--- projeto-06/main.js
+++ projeto-06/main.js
@@ -12,29 +12,44 @@ const buttonStop = document.querySelector('.stop')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
 let minutes
-const minutesDislay = document.querySelector('.minutes')
+const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 
+
+function resetControls() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+}
 
 // Event-driven
 // programação imperativa
 // callback
 
-function coutdown() {
-  setTimeout( function() {
+function countdown() {
+  setTimeout(function() {
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
     
-    if(seconds <= 0) {
-      seconds = 60
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+    if (minutes <= 0) {  
+      resetConstrols()
+      return
     }
 
-    secondsDisplay.textContent = seconds - 1
-    
-    coutdown()
+    if(seconds <=0) {
+      seconds = 2
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+    }
+
+
+  countdown()
   }, 1000)
+
 }
-
-
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
@@ -42,7 +57,7 @@ buttonPlay.addEventListener('click', function () {
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
   
-  coutdown()
+  countdown()
 })
 
 buttonPause.addEventListener('click', function () {
@@ -51,10 +66,7 @@ buttonPause.addEventListener('click', function () {
 })
 
 buttonStop.addEventListener('click', function () {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')
+  resetControls()
 })
 
 buttonSoundOn.addEventListener('click', function () {
@@ -69,5 +81,5 @@ buttonSoundOff.addEventListener('click', function () {
 
 buttonSet.addEventListener('click', function () {
   minutes = prompt("Quantos minutos?")
-  minutesDislay.textContent= minutes
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
 })
