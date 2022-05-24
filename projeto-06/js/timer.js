45d875b9fff9cf129547501a5ae0329add9ec820
Lucas Donay <lucas.donay@gmail.com>
2022-05-24 14:12:19 -0300

    finished project

--- projeto-06/js/timer.js
+++ projeto-06/js/timer.js
@@ -1,36 +1,52 @@
+import Sounds from "./sound.js"
+
+import {
+  buttonSoundOff,
+  buttonSoundOn,  
+} from "./elements.js"
+
 export default function Timer({ 
   minutesDisplay, 
   secondsDisplay, 
-  resetControls
+  resetControls,
+  sound,
 }) {
 
   let timerTimeOut
   let minutes = Number(minutesDisplay.textContent)
 
-  function updateDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  function updateDisplay(newMinutes, seconds) {
+    newMinutes = newMinutes === undefined ? minutes : newMinutes
+    seconds = seconds === undefined ? 0 : seconds
+    minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
     secondsDisplay.textContent = String(seconds).padStart(2, "0")
   }
 
   function reset() {
     updateDisplay(minutes, 0)
-    clearTimeout(timerTimeOut)
+    clearTimeout(timerTimeOut)    
   }
 
   function countdown(){
     timerTimeOut = setTimeout(function() {
       let seconds =  Number(secondsDisplay.textContent)
       let minutes = Number(minutesDisplay.textContent)
+      let isFinished = minutes <= 0 && seconds <= 0
 
       updateDisplay(minutes, 0)
 
-      if (minutes <= 0) {
+      if (isFinished) {
         resetControls()
+        updateDisplay()
+        Sounds().timeEnd()
+        sound.bgAudio.pause()       
+        buttonSoundOn.classList.add('hide')
+        buttonSoundOff.classList.remove('hide')
         return
       }
 
       if( seconds <= 0 ) {
-        seconds = 3
+        seconds = 6
         --minutes
       }
 
@@ -53,6 +69,6 @@ export default function Timer({
     reset,
     updateDisplay,
     updateMinutes,
-    hold
+    hold,    
   }
 }
