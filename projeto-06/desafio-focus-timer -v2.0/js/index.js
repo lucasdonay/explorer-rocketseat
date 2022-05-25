9d3c9208599809e52774c2e44072ef3963c55be4
Lucas Donay <lucas.donay@gmail.com>
2022-05-25 00:07:53 -0300

    finished contactor logic

--- projeto-06/desafio-focus-timer -v2.0/js/index.js
+++ projeto-06/desafio-focus-timer -v2.0/js/index.js
@@ -8,15 +8,27 @@ const buttonForest = document.querySelector('.forest')
 const buttonRain = document.querySelector('.rain')
 const buttonCoffee = document.querySelector('.coffee')
 const buttonFirePlace = document.querySelector('.fireplace')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
 
+function resetControls() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide') 
+}
 
-buttonPlay.addEventListener('click', function() {
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-  buttonSet.classList.add('hide')
-  buttonStop.classList.remove('hide')  
-})
+function updateTimerDisplay(minutes, seconds){
+  minutesDisplay.textContent = String(minutes).padStart(2, "0") 
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
 
+function reset() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
 
 function turnItOff(button) {
   if (button.classList.contains('active')) {
@@ -24,17 +36,51 @@ function turnItOff(button) {
   }
 }
 
+function countdown(){
+ timerTimeOut = setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    updateTimerDisplay(minutes, 0)
+    
+    if (minutes <= 0 & seconds <= 0) {
+      resetControls()
+      return
+    }
+
+    if (seconds <= 0 ) {
+      seconds = 4
+      --minutes
+    }
+    
+    countdown()
+
+    updateTimerDisplay(minutes, String(seconds - 1))
+
+  }, 1000)
+}
+  
+
+
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide') 
+
+  countdown()
+})
+
 buttonPause.addEventListener('click', function() {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
-  buttonSet.classList.add('hide')   
+  buttonSet.classList.add('hide')
+  clearTimeout(timerTimeOut)  
 })
 
 buttonStop.addEventListener('click', function() {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonStop.classList.add('hide')   
+  resetControls()
+  reset()
 })
 
 buttonForest.addEventListener('click', function() {
@@ -63,4 +109,9 @@ buttonFirePlace.addEventListener('click', function() {
   turnItOff(buttonCoffee)
   turnItOff(buttonForest)  
   turnItOff(buttonRain)     
+})
+
+buttonSet.addEventListener('click', function() {  
+  minutes = prompt("Quantos minutos?") || "0"
+  updateTimerDisplay(minutes, 0)
 })
