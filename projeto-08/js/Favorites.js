5149d47fb887992f149da4d39b4d190782ccee7b
Lucas Donay <lucas.donay@gmail.com>
2022-07-28 17:00:27 -0300

    localstorage

--- projeto-08/js/Favorites.js
+++ projeto-08/js/Favorites.js
@@ -1,23 +1,85 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
+    this.load()
+    
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) ||[]
+    console.log(this.entries);
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+    this.entries = filteredEntries
+    this.update()
   }
 }
 
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
+    
+    this.tbody = this.root.querySelector('table tbody')
     this.update()
   }
 
   update() {
-    this.removeAllTr()
+    this.removeAllTr()  
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+      
+      row.querySelector('.user img').alt = `Img de ${user.name}`
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user a').href= `https://github.com/${user.login}.png`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+       const Ok = confirm('Tem certeza que deseja deletar essa linha?')
+       if(Ok) {
+        this.delete(user)
+       }
+      }
+      
+
+      this.tbody.append(row)
+    });
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <tr>
+    <td class="user">
+      <img src="https://github.com/lucasdonay.png" alt="Imagem do Donay">
+      <a href="https://github.com/lucasdonay" target="_blank">
+        <p>Lucas Donay</p>
+        <span>donay</span>
+      </a>
+    </td>
+    <td class="repositories">
+      52
+    </td>
+    <td class="followers">
+      1000
+    </td>
+    <td>
+      <button class="remove">&times;</button>
+    </td>
+  </tr>
+    `
+    return tr;
   }
 
   removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
-
-    tbody.querySelectorAll('tr').forEach(tr => {
+      this.tbody.querySelectorAll('tr').forEach(tr => {
       tr.remove()
     });
   }
