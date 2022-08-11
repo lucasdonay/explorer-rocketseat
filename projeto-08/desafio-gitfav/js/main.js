3f0e41346c9ad19d9b81a692948088b43ec4d7b1
Lucas Donay <lucas.donay@gmail.com>
2022-08-11 15:26:26 -0300

    starting project

---
+++ projeto-08/desafio-gitfav/js/main.js
@@ -0,0 +1,9 @@
+const but = document.querySelector('button')
+
+const tableBorder = document.querySelector('tbody')
+
+
+but.addEventListener('click', function() {
+   tableBorder.classList.remove('clean-table');
+  console.log("teste");
+ })
