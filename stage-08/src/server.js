8ec9bb9a3f280574367e4901b7f1fec3e9d1aa0e
Lucas Donay <lucas.donay@gmail.com>
2022-09-06 16:01:32 -0300

    continuing

---
+++ stage-08/src/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`
+  Mensagem ID: ${id}.
+  Para o usuario  ${user}
+  `)
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+  
+  response.send(`
+  Pagina: ${page}.
+  Mostrar: ${limit}
+  `)
+});
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
