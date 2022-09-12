94b5e08efed0169fa77e463dca75413bbc766c58
Lucas Donay <lucas.donay@gmail.com>
2022-09-12 17:53:37 -0300

    continuing the study

--- stage-08/src/server.js
+++ stage-08/src/server.js
@@ -1,24 +1,11 @@
 const express = require("express")
 
+const routes = require("./routes")
+
 const app = express()
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
-
-  response.send(`
-  Mensagem ID: ${id}.
-  Para o usuario  ${user}
-  `)
-});
-
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-  
-  response.send(`
-  Pagina: ${page}.
-  Mostrar: ${limit}
-  `)
-});
+app.use(routes)
 
 const PORT = 3333;
 
