bee2ff10022bf7d0686fe38ced2588c2c33cf36d
Lucas Donay <lucas.donay@gmail.com>
2022-09-13 16:31:40 -0300

    continuing stage 8

--- stage-08/src/controllers/UsersController.js
+++ stage-08/src/controllers/UsersController.js
@@ -1,9 +1,18 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
   create(request, response) {
     
   const { name, email, password} = request.body
+
+   if(!name) {
+    throw new AppError("Voce precisa coloca o nome")
+   }
+   if(!email) {
+    throw new AppError("Voce precisa coloca o email")
+   }
     
-  response.json({ name, email, password})
+  response.status(201).json({ name, email, password})
   }
 
 }
--- stage-08/src/routes/users.routes.js
+++ stage-08/src/routes/users.routes.js
@@ -4,8 +4,17 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router()
 
+function myMiddleware(request, response, next) {
+console.log("Voce passou por aqui");
+
+if (!request.body.isAdmin) {
+   response.json({ message: "VOCE NAO ESTA AUTORIZADO" });
+}
+next()
+}
+
 const usersController = new UsersController()
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/", myMiddleware, usersController.create);
 
 module.exports = usersRoutes;
