cecc759001938814b698e8e60c0b8ae1b72a34b0
Lucas Donay <lucas.donay@gmail.com>
2022-11-07 17:53:46 -0300

    iniciando logica

--- projeto-08/desafio-gitfav/js/Favorites.js
+++ projeto-08/desafio-gitfav/js/Favorites.js
@@ -1,34 +1,39 @@
 //class pra ter a logica
 export class Favorites {
   constructor(root) {
-  this.root = document.querySelector(root)
-  }
-  removeBg()
+  this.root = document.querySelector(root)  
+}  
 }
-
-
-
-
 
 //class qye vai criar a visualizacao e eventos do html
 
 export class FavoritesView extends Favorites{
   constructor(root) {
     super(root)
-  }
+   /*  this.removeBg() */
+    this.update()   
+ }
 
-
-  removeBg() {
-    
-    const but = this.root.querySelector('button')
-
-    const tbody = this.root.querySelector('table tbody')
-
+/*   removeBg() {    
+    const but = document.querySelector('.teste1')
+    const tbody = document.querySelector('table tbody')
 
     but.addEventListener('click', function() {
-      tbody.classList.remove('.clean-table');
+      tbody.classList.remove('clean-table');
       console.log("teste");
     })
 
+  } */
+
+  update() {
+    const tbody = this.root.querySelector('table tbody')
+
+    tbody.querySelectorAll('tr')
+    .forEach((tr) => {
+      if(tr === tr) {
+        tbody.classList.remove('clean-table')
+        return
+      }
+    })
   }
 }
