ee25a1664720798a14c0d0edad296d99bb923d15
Lucas Donay <lucas.donay@gmail.com>
2022-11-23 17:53:56 -0300

    continuando estudos

--- stage-08-react/rocketnotes/src/pages/Details/index.jsx
+++ stage-08-react/rocketnotes/src/pages/Details/index.jsx
@@ -1,15 +1,22 @@
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
 import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello Word</h1>
-      <p>teste</p>
-      <Button title="Entrar" loading/>
-      <Button title="Teste"/>
-      <Button title="Castrar"/>
-      <Button title="Logar"/>
+      <Header />
+
+      <Section title="Links Uteis">  
+          <Links>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          </Links>
+      </Section>
+
+      <Button title="Voltar"/>
     </Container>
   )
 }
