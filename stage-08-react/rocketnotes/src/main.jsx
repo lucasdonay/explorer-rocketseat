d63ea23fa8666a8e1a5a5677ef05240296baafd4
Donay <lucas.donay@gmail.com>
2022-11-25 23:09:58 -0300

    start layout home

--- stage-08-react/rocketnotes/src/main.jsx
+++ stage-08-react/rocketnotes/src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
+import { Home } from './pages/Home'
 
 import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider  theme={theme}>
-    <GlobalStyles />
-     <Details />    
+      <GlobalStyles />
+     <Home />    
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
